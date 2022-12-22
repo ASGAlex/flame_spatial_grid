3530af531ae6eaa4df871c15d39d62bd2f3c3104
Alexey Volkov <asgalex@gmail.com>
2022-12-22 15:30:48 +0300

    feat: add example map adn example world

--- examples/assets/tiles/tileset.tsx
+++ examples/assets/tiles/tileset.tsx
@@ -2,6 +2,7 @@
 <tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="8" tileheight="8" tilecount="8" columns="4">
  <image source="../images/retro_tiles.png" width="32" height="16"/>
  <tile id="0" class="Brick"/>
+ <tile id="3" class="Tree"/>
  <tile id="4" class="Water">
   <animation>
    <frame tileid="4" duration="500"/>
