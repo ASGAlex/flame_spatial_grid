62fdb1d85022a04b3510c032315b41b639f36cd0
Alexey Volkov <asgalex@gmail.com>
2023-01-15 23:32:57 +0300

    doc: Cell description

--- lib/src/core/cell.dart
+++ lib/src/core/cell.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_if_null_to_convert_nulls_to_bools
+// ignore_for_file: use_if_null_to_convert_nulls_to_bools, comment_references
 
 import 'dart:collection';
 
@@ -22,14 +22,24 @@ enum CellState {
   inactive,
 
   /// This kind of cells are very far from player. Usually, player did there
-  /// at past but then left these cells. Fot such cells [Component.upateTree] is
+  /// at past but then left these cells. Fot such cells [Component.updateTree] is
   /// omitted to preserve CPU resources. See [HasGridSupport] for details.
   suspended
 }
 
+/// Cell is the rect (quad) of game space, connected with it's neighbors at
+/// left, right, top and bottom.
+/// This class stores cell information and automates new cells creation,makes
+/// easier querying cell's components for collision detection system.
+///
+/// Usually you do not need to use any of it's methods. But you absolutely
+/// should to ensure that component's [HasGridSupport.currentCell] is not null.
 class Cell {
-  Cell(
-      {required this.spatialGrid, required this.rect, bool suspended = false}) {
+  Cell({
+    required this.spatialGrid,
+    required this.rect,
+    bool suspended = false,
+  }) {
     center = rect.center.toVector2();
     spatialGrid.cells[rect] = this;
 
@@ -70,21 +80,29 @@ class Cell {
   bool _remove = false;
   final SpatialGrid spatialGrid;
   final Rect rect;
+
+  /// Cell's central point.
   late final Vector2 center;
 
   final _state = ValueNotifier<CellState>(CellState.active);
 
+  /// Collection of component currently places inside this cell.
+  /// Should not be modified manually!
   final components = HashSet<HasGridSupport>();
 
   SpatialGridBroadphase get broadphase =>
       spatialGrid.game.collisionDetection.broadphase;
 
+  /// Left cell from current, or null
   Cell? get rawLeft => _rawLeft?._remove == true ? null : _rawLeft;
 
+  /// Right cell from current, or null
   Cell? get rawRight => _rawRight?._remove == true ? null : _rawRight;
 
+  /// Top cell from current, or null
   Cell? get rawTop => _rawTop?._remove == true ? null : _rawTop;
 
+  /// Bottom cell from current, or null
   Cell? get rawBottom => _rawBottom?._remove == true ? null : _rawBottom;
 
   set rawLeft(Cell? value) {
@@ -108,29 +126,45 @@ class Cell {
   Cell? _rawTop;
   Cell? _rawBottom;
 
+  /// A time in microseconds from the moment when cell's state was changed into
+  /// [CellState.suspended].
+  /// See [updateComponentsState] too.
   double beingSuspendedTimeMicroseconds = 0;
 
   final _cachedRects = <_CellCreationContext, Rect>{};
 
+  /// Existing left cell if not scheduled for removal, otherwise null
   Cell? get leftChecked => rawLeft ??= _checkCell(_CellCreationContext.left);
 
+  /// Existing right cell if not scheduled for removal, otherwise null
   Cell? get rightChecked => rawRight ??= _checkCell(_CellCreationContext.right);
 
+  /// Existing top cell if not scheduled for removal, otherwise null
   Cell? get topChecked => rawTop ??= _checkCell(_CellCreationContext.top);
 
+  /// Existing bottom cell if not scheduled for removal, otherwise null
   Cell? get bottomChecked =>
       rawBottom ??= _checkCell(_CellCreationContext.bottom);
 
+  /// Existing or new left cell
   Cell get left => rawLeft ??= _createCell(_CellCreationContext.left);
 
+  /// Existing or new right cell
   Cell get right => rawRight ??= _createCell(_CellCreationContext.right);
 
+  /// Existing or new top cell
   Cell get top => rawTop ??= _createCell(_CellCreationContext.top);
 
+  /// Existing or new bottom cell
   Cell get bottom => rawBottom ??= _createCell(_CellCreationContext.bottom);
 
+  /// Most important cell's parameter, controls cell's lifecycle and lifecycle
+  /// of components inside the cell.
+  /// See [CellState] for detailed description of states meaning.
+  /// See [updateComponentsState] to understand a part of internal mechanics.
   CellState get state => _state.value;
 
+  @internal
   CellState tmpState = CellState.active;
 
   set state(CellState value) {
@@ -199,6 +233,9 @@ class Cell {
     }
   }
 
+  /// The cell should to remove itself from all neighbours and [spatialGrid],
+  /// remove all cell's components from game tree and dispose [state]'s
+  /// listeners.
   void remove() {
     _remove = true;
     rawLeft?.rawRight = null;
@@ -254,6 +291,10 @@ class Cell {
     return newRect;
   }
 
+  /// Get cell's left, right, top and bottom neighbours, if exists.
+  /// Also includes left-top, right-top, right-bottom and left-bottom
+  /// neighbours.
+  /// Useful for collision detection system.
   List<Cell> get neighbours {
     final list = <Cell>[];
     if (rawLeft != null) {
