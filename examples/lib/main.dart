99ba289645306a569597a022c2cbf36ad0d6fd0d
Alexey Volkov <asgalex@gmail.com>
2023-01-16 02:56:54 +0300

    fix: incorrect game for demo

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: GameWidget(game: MinimalGame()),
+      home: GameWidget(game: SpatialGridExample()),
     );
   }
 }
